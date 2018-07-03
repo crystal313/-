8e6ecd534aaca9ef7da54408bfebc60db1dbb87a
crystal313 <1027184987@qq.com>
2018-07-03 09:08:36 +0800

    biaoqian

--- 注册.js
+++ 注册.js
@@ -14,6 +14,56 @@ function next(){
 	}
 
 	document.getElementById("finish").style.display = "block";
-
-
 }
+function finish1(){
+	var obj = document.getElementsByName("Sex");
+	var gery = document.getElementsByClassName("greybox");
+	var female = document.getElementsByClassName("female");
+	var male = document.getElementsByClassName("male");
+	
+	for(var j = 0;j<gery.length;j++){
+
+		gery[j].style.display = "none";		
+	}
+	document.getElementById("biglabel").style.display="block";
+    for(var i=0; i<obj.length; i ++){
+        if(obj[i].checked===true){
+           var sex = obj[i].value;
+		   if(sex==="male"){
+			   for(var m=0;m<male.length;m++){
+				   male[m].style.display="block";
+			   }
+		   }else if(sex==="female"){
+			   for(var f=0;f<male.length;f++){
+				   female[f].style.display="block";
+			   }
+		   }else if(sex==="secret"){
+			   
+		   }			
+        }		
+    }
+	
+	
+	
+	
+}
+
+$(document).ready(function(){
+	
+});
+
+
+$(function(){
+$("a").not("#finish1").click(function(){
+	    
+	    var a_choosed = $(this);
+	    
+	    $(a_choosed).toggleClass("unchooesd");
+	    $(a_choosed).toggleClass("choosed");
+	    $(a_choosed).find(">h3").toggleClass("delete");
+	
+	
+});
+});
+
+
